e32450d2b9207f9eeeae8813801ddba6ec80864c
Atharva Unde <atharv.unde59@gmail.com>
2020-07-22 09:14:51 +0530

    Removed Firebase Config Values

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,14 @@
 import firebase from "firebase";
 
 const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyDZ4mJgVJvMbbBwWBMMRsp-tx8hF1POb8o",
-    authDomain: "instagram-clone-react-89ef3.firebaseapp.com",
-    databaseURL: "https://instagram-clone-react-89ef3.firebaseio.com",
-    projectId: "instagram-clone-react-89ef3",
-    storageBucket: "instagram-clone-react-89ef3.appspot.com",
-    messagingSenderId: "146409917536",
-    appId: "1:146409917536:web:40c369c94101f243e94705",
-    measurementId: "G-4HR9M7V32M"
+     apiKey: "Axxxxxxxxxxxxxxxxxxx",
+     authDomain: "xxxxxx.firebaseapp.com",
+     databaseURL: "https://xxxxxxxx.firebaseio.com",
+     projectId: "xxxxxxxxx",
+     storageBucket: "xxxxxxx.appspot.com",
+     messagingSenderId: "xxxxxxxxxxx",
+     appId: "1:xxxxxxxxx:web:xxxxxxx",
+     measurementId: "xxxxxxxxx"
 });
 
 const db = firebaseApp.firestore();
